3ea050e9b3a689951964fe85bd1788d6d031d6fc
byungjuchae <63639887+byoungjuchae@users.noreply.github.com>
2025-05-13 13:31:06 +0900

    Update agent.py

--- agent.py
+++ agent.py
@@ -7,7 +7,7 @@ from langchain_core.runnables import RunnablePassthrough
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_core.tools import tool
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_openai import ChatOpenAI
 from langgraph.prebuilt import create_react_agent
 from langgraph.graph import StateGraph, END
 from dotenv import load_dotenv
@@ -18,6 +18,10 @@ import uuid
 import streamlit as st
 from cover.cover_agent import coverwriter
 from job_rag.job_rag_agent import RAG
+from CV_writing.CV_re import cv_write
+from fastapi import FastAPI, UploadFile, File
+import requests
+import shutil
 from fastapi import FastAPI, UploadFile, File
 import requests
 import json
@@ -28,14 +32,15 @@ app = FastAPI()
 
 load_dotenv()
 
-os.environ['LANGCHAIN_API_KEY'] = os.getenv('LANGCHAIN_API_KEY')
-os.environ['LANGCHAIN_TRACING_V2'] = "true"
-os.environ['LANGCHAIN_PROJECT'] = "CRAG"
-os.environ['LANGCHAIN_ENDPOINT'] = "https://api.smith.langchain.com"
+# os.environ['LANGCHAIN_API_KEY'] = os.getenv('LANGCHAIN_API_KEY')
+# os.environ['LANGCHAIN_TRACING_V2'] = "true"
+# os.environ['LANGCHAIN_PROJECT'] = "CRAG"
+# os.environ['LANGCHAIN_ENDPOINT'] = "https://api.smith.langchain.com"
 
 GEMINI_KEY = os.getenv("GEMINI_KEY")
 INSERT_TOKEN = os.getenv("INSERT_TOKEN")
 
+llm = ChatGoogleGenerativeAI(model='gemini2.0-flash',google_api_key=GEMINI_KEY)
 
 class State(BaseModel):
     
@@ -47,46 +52,21 @@ class State(BaseModel):
     result : str
     score : str
     
-llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", google_api_key=GEMINI_KEY)
+
 docs = PyPDFLoader('./pdf/CV.pdf').load()
 
 def make_config():
     return {"configurable": {"thread_id": str(uuid.uuid4())}}
 
 
-
-@app.post('/job_posting')
-def get_url():
-    headers = {
-    'X-RestLi-Protocol-Version': '2.0.0',
-    'Linkedin-Version': '202503',
-    'Authorization': f'Bearer {INSERT_TOKEN}'  
-    }
-
-    job = "AI%02Engineer"
-    start_day = "12"
-    start_month = "05"
-    start_year = "2025"
-    end_day = "13"
-    end_month = "05"
-    end_year = "2025"
-
-    url = f"https://api.linkedin.com/rest/jobLibrary?q=criteria&keyword={job}&dateRange=(start:(day:{start_day},month:{start_month},year:{start_year}),end:(day:{end_day},month:{end_month},year:{end_year}))&start=100&count=5"""
-    response = requests.get(url,headers=headers)
- 
-    docs = []
-    for i in range(len(response.json()['elements'])):
-        name = os.path.basename(response.json()['elements'][i]['jobPostingUrl'])
-        print(i)
-        docs.append(response.json()['elements'][i])
-       
-    return docs
-
-
 @app.post('/pdf')
-async def pdf_load(pdf_file: UploadFile = File(...)):
-    contents = await pdf_file.read()
-    docs = PyPDFLoader(contents).load()
+async def pdf(pdf_file: UploadFile = File(...)):
+    file_path = os.path.join(UPLOAD_DIR, pdf_file.filename)
+
+    with open(file_path, "wb") as f:
+        shutil.copyfileobj(pdf_file.filename, f)
+
+    docs = PyPDFLoader(file_path).load()
     return {"message": "PDF 처리 완료", "num_pages": len(docs)}
 
 @app.post('/chat')
@@ -94,7 +74,7 @@ async def chat():
 
     print("📄 Cover Letter Chatbot Ready — 'exit' 입력 시 종료")
     config = {"configurable": {"thread_id": "53"}}
-    agents= create_react_agent(llm,tools=[coverwriter,RAG],
+    agents= create_react_agent(llm,tools=[coverwriter,cv_write],
     prompt=("You're a helpful assistant designed to use tools effectively. When a question comes in, don't ask for permission—if it looks like a tool should be used, just go ahead and use it."
             "if you want assistance crafting your cover letter, execute coverwriter."
             "if you want to find specific company, execute RAG"
@@ -114,7 +94,7 @@ async def chat():
             print("\nBot COVER LETTER:\n")
             print(chunk)
 
-docs = [doc.page_content for doc in docs]
+
 
 
 
